441634242777eaf65c21be9737b2d8de06d25316
packyan <packyande@gmail.com>
2018-04-10 23:06:06 +0800

    change platform

--- Socket/Linux_server.c
+++ Socket/Linux_server.c
@@ -5,8 +5,18 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
-
+#include<unistd.h>
+#include <iostream>
 #define MAXLINE 4096
+using std::cout;
+using std::endl;
+#pragma pack(push, 1) // 取消内存大小自动对齐
+struct face_fit_msg{
+unsigned char packages_head[2] ={0xff, 0xfe};
+double face_fit_data[4];
+unsigned char package_end[3] = {0xbe,0xef,'\0'};
+};
+#pragma pack(pop)
 
 int main(int argc, char** argv)
 {
@@ -14,7 +24,7 @@ int main(int argc, char** argv)
     struct sockaddr_in   servaddr;
     char    buff[4096];
     int     n;
-
+	face_fit_msg face_fit_recv;
     if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
     printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
     exit(0);
@@ -41,9 +51,12 @@ int main(int argc, char** argv)
         printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
         continue;
     }
-    n = recv(connfd, buff, MAXLINE, 0);
-    buff[n] = '\0';
-    printf("recv msg from client: %s\n", buff);
+	memset(&face_fit_recv, 0x00, sizeof(face_fit_msg));
+	recv(connfd, (char*)&face_fit_recv, sizeof(face_fit_msg), 0);
+	cout << face_fit_recv.face_fit_data[0] << endl;
+/*    n = recv(connfd, buff, MAXLINE, 0);*/
+/*    buff[n] = '\0';*/
+/*    printf("recv msg from client: %s\n", buff);*/
 
  	if(send(connfd, "Hello,you are connected!\n", 26,0) == -1)  
 	{
